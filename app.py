b56c58536d4f980fd64c7b9053b4c15e69d8f366
ItsAqie <husnulyaqin.office@gmail.com>
2026-06-17 13:09:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ api_key_bersih = api_key_input.strip()
 # Aktivasi AI
 try:
     genai.configure(api_key=api_key_bersih)
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-pro-vision')
 except Exception as e:
     st.error(f"Kunci tidak valid: {e}")
     st.stop()
